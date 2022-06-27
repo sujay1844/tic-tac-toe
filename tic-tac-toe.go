bb0ce7c1fd010078ebd08874befe8cf5f4375628
Sujay1844 <sujay1844@protonmail.com>
2022-06-27 11:44:41 +0530

    Added winning

--- tic-tac-toe.go
+++ tic-tac-toe.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	//"bufio"
 	"fmt"
 	"os"
 	"time"
 )
 
 func main(){
+	// Print game info
 	fmt.Println("TIC TAC TOE")
 	fmt.Println("To choose your square press a number from 1 to 9 like a numpad")
+	fmt.Println("Enter your chosen number and press enter")
+	// Wait 1 sec for user to read
 	time.Sleep(time.Duration(2000) * time.Millisecond)
+
+	// Variable initializing
 	game := [9]int{0,0,0,0,0,0,0,0,0}
-	fmt.Println(game)
 	x_turn := true
 	var choice int
 	number_of_moves_left := 0
 	for {
-		// Clear screen
-		fmt.Println("\033[H\033[2J")
+		clrscr()
+
 		// Print the grid
 		print_game(game)
+
 		if x_turn {
+			// toggle x_turn to give to the other player
 			x_turn = false
 			fmt.Print("X's turn: ")
 			fmt.Scan(&choice)
@@ -40,6 +45,7 @@ func main(){
 				x_turn = true
 			}
 		} else {
+			// toggle x_turn to give to the other player
 			x_turn = true
 			fmt.Print("O's turn: ")
 			fmt.Scan(&choice)
@@ -64,8 +70,19 @@ func main(){
 		for i :=0 ; i<9 ; i++ {
 			if game[i] == 0 { number_of_moves_left += 1 }
 		}
-		if number_of_moves_left == 0 {
-			fmt.Println("\033[H\033[2J")
+
+		if check_win(game) == 1 {
+			clrscr()
+			print_game(game)
+			fmt.Println("X won the game")
+			os.Exit(0)
+		} else if check_win(game) == 2 {
+			clrscr()
+			print_game(game)
+			fmt.Println("O won the game")
+			os.Exit(0)
+		} else if number_of_moves_left == 0 {
+			clrscr()
 			print_game(game)
 			fmt.Println("It's a tie")
 			os.Exit(0)
@@ -74,19 +91,23 @@ func main(){
 }
 
 func print_game(grid [9]int) {
-	fmt.Println("+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[0]) + "  |  " + disp(grid[1]) + "  |  " + disp(grid[2]) + "  |")
-	fmt.Println("+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[3]) + "  |  " + disp(grid[4]) + "  |  " + disp(grid[5]) + "  |")
-	fmt.Println("+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[6]) + "  |  " + disp(grid[7]) + "  |  " + disp(grid[8]) + "  |")
-	fmt.Println("+-----+-----+-----+")
+	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
+	fmt.Println("|  " + disp(grid[0]) + "  |  " + disp(grid[1]) + "  |  " + disp(grid[2]) + "  |		|  1  |  2  |  3  |")
+	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
+	fmt.Println("|  " + disp(grid[3]) + "  |  " + disp(grid[4]) + "  |  " + disp(grid[5]) + "  |		|  4  |  5  |  6  |")
+	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
+	fmt.Println("|  " + disp(grid[6]) + "  |  " + disp(grid[7]) + "  |  " + disp(grid[8]) + "  |		|  7  |  8  |  9  |")
+	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
 }
 
 func disp(n int) string{
 	if n == 1 { return "X" }
 	if n == 2 { return "O" }
 	return " "
+}
+
+func clrscr() {
+	fmt.Println("\033[H\033[2J")
 }
 
 func edit_grid(grid [9]int, pos int, player string) [9]int{
@@ -99,64 +120,32 @@ func edit_grid(grid [9]int, pos int, player string) [9]int{
 }
 
 func check_win(g[9]int) int{
-	// Checking if O has won
+	// Checking if X has won
 
 	// Checking if horizontal rows are equal
-	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 1 {
-		return 1
-	}
-	if g[3] == g[4] && g[4] == g[5] && g[5] != 0 && g[3] == 1 {
-		return 1
-	}
-	if g[6] == g[7] && g[7] == g[8] && g[2] != 0 && g[6] == 1 {
-		return 1
-	}
+	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 1 { return 1 } else
+	if g[3] == g[4] && g[4] == g[5] && g[5] != 0 && g[3] == 1 { return 1 } else
+	if g[6] == g[7] && g[7] == g[8] && g[2] != 0 && g[6] == 1 { return 1 } else
 	// Checking if vertical columns are equal
-	if g[0] == g[3] && g[3] == g[6] && g[6] != 0 && g[0] == 1 {
-		return 1
-	}
-	if g[1] == g[4] && g[4] == g[7] && g[7] != 0 && g[1] == 1 {
-		return 1
-	}
-	if g[2] == g[5] && g[5] == g[8] && g[8] != 0 && g[2] == 1 {
-		return 1
-	}
+	if g[0] == g[3] && g[3] == g[6] && g[6] != 0 && g[0] == 1 { return 1 } else
+	if g[1] == g[4] && g[4] == g[7] && g[7] != 0 && g[1] == 1 { return 1 } else
+	if g[2] == g[5] && g[5] == g[8] && g[8] != 0 && g[2] == 1 { return 1 } else
 	// Checking if diagonals are equal
-	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 1 {
-		return 1
-	}
-	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 1 {
-		return 1
-	}
+	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 1 { return 1 } else
+	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 1 { return 1 } else
 
-	//Checking if X has won
+	//Checking if O has won
 
 	// Checking if horizontal rows are equal
-	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 1 {
-		return 2
-	}
-	if g[3] == g[4] && g[4] == g[5] && g[5] != 0 && g[3] == 1 {
-		return 2
-	}
-	if g[6] == g[7] && g[7] == g[8] && g[2] != 0 && g[6] == 1 {
-		return 2
-	}
+	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 2 { return 2 } else
+	if g[3] == g[4] && g[4] == g[5] && g[5] != 0 && g[3] == 2 { return 2 } else
+	if g[6] == g[7] && g[7] == g[8] && g[2] != 0 && g[6] == 2 { return 2 } else
 	// Checking if vertical columns are equal
-	if g[0] == g[3] && g[3] == g[6] && g[6] != 0 && g[0] == 1 {
-		return 2
-	}
-	if g[1] == g[4] && g[4] == g[7] && g[7] != 0 && g[1] == 1 {
-		return 2
-	}
-	if g[2] == g[5] && g[5] == g[8] && g[8] != 0 && g[2] == 1 {
-		return 2
-	}
+	if g[0] == g[3] && g[3] == g[6] && g[6] != 0 && g[0] == 2 { return 2 } else
+	if g[1] == g[4] && g[4] == g[7] && g[7] != 0 && g[1] == 2 { return 2 } else
+	if g[2] == g[5] && g[5] == g[8] && g[8] != 0 && g[2] == 2 { return 2 } else
 	// Checking if diagonals are equal
-	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 1 {
-		return 2
-	}
-	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 1 {
-		return 2
-	}
+	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 2 { return 2 } else
+	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 2 { return 2 }
 	return 0
 }
