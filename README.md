88718bfb22dc61917be7dc8a7ef174730ee12fe8
Sujay1844 <sujay1844@protonmail.com>
2022-06-27 11:59:29 +0530

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tic-tac-toe
 A local Tic Tac Toe game using Go
+
+- To play, run tic-tac-toe.exe in Command Prompt
+
+I hope the code is well documented ;)
--- tic-tac-toe.go
+++ tic-tac-toe.go
@@ -11,7 +11,7 @@ func main(){
 	fmt.Println("TIC TAC TOE")
 	fmt.Println("To choose your square press a number from 1 to 9 like a numpad")
 	fmt.Println("Enter your chosen number and press enter")
-	// Wait 1 sec for user to read
+	// Wait 2 sec for user to read
 	time.Sleep(time.Duration(2000) * time.Millisecond)
 
 	// Variable initializing
@@ -28,20 +28,26 @@ func main(){
 		if x_turn {
 			// toggle x_turn to give to the other player
 			x_turn = false
+
+			//Taking user input
 			fmt.Print("X's turn: ")
 			fmt.Scan(&choice)
 			if choice >= 1 && choice <=9 {
+
+				// Making sure that the chosen square is empty
 				if game[choice-1] == 0 {
 					game = edit_grid(game, choice-1, "X")
 				} else {
 					fmt.Println("Spot already taken. Try again")
 					time.Sleep(time.Duration(1000) * time.Millisecond)
+					// Giving the player another chance if input was wrong
 					x_turn = true
 				}
 			} else {
 				fmt.Println("Invalid input")
 				fmt.Println("There are only 9 squares, buddy")
 				time.Sleep(time.Duration(1000) * time.Millisecond)
+				// Giving the player another chance if input was wrong
 				x_turn = true
 			}
 		} else {
@@ -50,38 +56,44 @@ func main(){
 			fmt.Print("O's turn: ")
 			fmt.Scan(&choice)
 			if choice >= 1 && choice <=9 {
+				// Making sure that the chosen square is empty
 				if game[choice-1] == 0 {
 					game = edit_grid(game, choice-1, "O")
 				} else {
 					fmt.Println("Spot already taken. Try again")
 					time.Sleep(time.Duration(1000) * time.Millisecond)
-					time.Sleep(1)
+					// Giving the player another chance if input was wrong
 					x_turn = false
 				}
 			} else {
 				fmt.Println("Invalid input")
 				fmt.Println("There are only 9 squares, buddy")
 				time.Sleep(time.Duration(1000) * time.Millisecond)
+				// Giving the player another chance if input was wrong
 				x_turn = false
 			}
 		}
 
+		// Counting number of moves left
 		number_of_moves_left = 0
 		for i :=0 ; i<9 ; i++ {
 			if game[i] == 0 { number_of_moves_left += 1 }
 		}
 
 		if check_win(game) == 1 {
+			// X won
 			clrscr()
 			print_game(game)
 			fmt.Println("X won the game")
 			os.Exit(0)
 		} else if check_win(game) == 2 {
+			// O won
 			clrscr()
 			print_game(game)
 			fmt.Println("O won the game")
 			os.Exit(0)
 		} else if number_of_moves_left == 0 {
+			// Tie match
 			clrscr()
 			print_game(game)
 			fmt.Println("It's a tie")
@@ -90,13 +102,19 @@ func main(){
 	}
 }
 
-func print_game(grid [9]int) {
+// A function to print the game on the terminal along with reference grid
+func print_game(g [9]int) {
+	// Using g for grid to shorten code
+	fmt.Println("TIC TAC TOE")
+	fmt.Println("To choose your square press a number from 1 to 9 like a numpad")
+	fmt.Println("Enter your chosen number and press enter")
+	fmt.Println("					Reference")
 	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[0]) + "  |  " + disp(grid[1]) + "  |  " + disp(grid[2]) + "  |		|  1  |  2  |  3  |")
+	fmt.Println("|  " + disp(g[0]) + "  |  " + disp(g[1]) + "  |  " + disp(g[2]) + "  |		|  1  |  2  |  3  |")
 	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[3]) + "  |  " + disp(grid[4]) + "  |  " + disp(grid[5]) + "  |		|  4  |  5  |  6  |")
+	fmt.Println("|  " + disp(g[3]) + "  |  " + disp(g[4]) + "  |  " + disp(g[5]) + "  |		|  4  |  5  |  6  |")
 	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
-	fmt.Println("|  " + disp(grid[6]) + "  |  " + disp(grid[7]) + "  |  " + disp(grid[8]) + "  |		|  7  |  8  |  9  |")
+	fmt.Println("|  " + disp(g[6]) + "  |  " + disp(g[7]) + "  |  " + disp(g[8]) + "  |		|  7  |  8  |  9  |")
 	fmt.Println("+-----+-----+-----+		+-----+-----+-----+")
 }
 
@@ -107,6 +125,7 @@ func disp(n int) string{
 }
 
 func clrscr() {
+	// Clearing the terminal with ANSI Escape codes
 	fmt.Println("\033[H\033[2J")
 }
 
@@ -120,7 +139,11 @@ func edit_grid(grid [9]int, pos int, player string) [9]int{
 }
 
 func check_win(g[9]int) int{
-	// Checking if X has won
+	// Using g for grid to shorten code
+
+	//----------------------//
+	// Checking if X has won//
+	//----------------------//
 
 	// Checking if horizontal rows are equal
 	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 1 { return 1 } else
@@ -134,7 +157,9 @@ func check_win(g[9]int) int{
 	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 1 { return 1 } else
 	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 1 { return 1 } else
 
-	//Checking if O has won
+	//---------------------//
+	//Checking if O has won//
+	//---------------------//
 
 	// Checking if horizontal rows are equal
 	if g[0] == g[1] && g[1] == g[2] && g[2] != 0 && g[0] == 2 { return 2 } else
@@ -147,5 +172,6 @@ func check_win(g[9]int) int{
 	// Checking if diagonals are equal
 	if g[0] == g[4] && g[4] == g[8] && g[8] != 0 && g[0] == 2 { return 2 } else
 	if g[2] == g[4] && g[4] == g[6] && g[6] != 0 && g[2] == 2 { return 2 }
+
 	return 0
 }
